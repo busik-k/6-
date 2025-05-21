d7ee7872d3dac07aba2f3e52947832a462d34752
busik-k <kamaleeva.kamilya@yandex.ru>
2025-05-22 00:17:48 +0300

    Create thirdarr.c

---
+++ thirdarr.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  // Creating array with length (3) and inserting into it nums
+  int nums[3] = {1, 2, 3};
+  
+  // Printing nums array
+  for (int i = 0; i < 3; i++) {
+    printf("%d\n", nums[i]);
+  }
+  
+  return 0;
+}
